64723a8f836ad32e1c43d75653aeaab41485a36c
Herby <herbyosbourne31@gmail.com>
2022-05-27 17:25:52 -0500

    update webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,56 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: { sourceMap: true },
+              },
+              {
+                loader: 'sass-loader',
+                options: { sourceMap: true },
+              },
+            ],
+          }),
+          // use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map', //cheap-module-eval-source-map
+    devServer: {
+      // hot: true,
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+    plugins: [
+      // Enable the plugin
+      CSSExtract,
+      // new webpack.HotModuleReplacementPlugin(),
+    ],
+  };
+};
+
+/* module.exports = {
   entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
@@ -31,4 +80,4 @@ module.exports = {
   //   // Enable the plugin
   //   new webpack.HotModuleReplacementPlugin(),
   // ],
-};
+}; */
